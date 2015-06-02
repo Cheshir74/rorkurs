06e18047d8574fc50937920e61767b62c3a608f4
ror <total@techbuben.ru>
2015-06-02 17:20:06 +0500

    fix remarks

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
-has_many :answers
+has_many :answers, :dependent :destroy
 
 validates :title, :body, presence: true
+validates_length_of :title, in: 5..140
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe Question, type: :model do
 	it { should validate_presence_of :title }
 	it { should validate_presence_of :body }
 	it { should have_many :answers }
+	it { should validate_length_of(:title).is_at_least(5).is_at_most(140) }
+
 end
