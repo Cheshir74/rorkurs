eea71fbe977895c6596db48ec246176d580c5b12
ror <total@techbuben.ru>
2015-07-16 22:32:55 +0500

    final fix remarks

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Answer, type: :model do
     let!(:question) { create(:question, user: user)}
     let(:answer1) { create(:answer, question: question, best: false)}
     let(:answer2) { create(:answer, question: question, best: false)}
+    let!(:answer) { create(:answer, question: question, best: true)}
 
 
     it 'set best answer' do
@@ -21,11 +22,10 @@ RSpec.describe Answer, type: :model do
     end
 
     it 'other answer set best' do
-      answer1.set_best
       answer2.set_best
-      answer1.reload
+      answer.reload
 
-      expect(answer1.best).to eq false
+      expect(answer.best).to eq false
       expect(answer2.best).to eq true
     end
   end
