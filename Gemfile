b2b4d0d8daa464a0db2bcff967ae1e86370a0dea
ror <total@techbuben.ru>
2015-07-16 23:41:01 +0500

    Attachment model

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'unicorn'
 gem 'slim-rails'
 gem 'devise'
+gem 'carrierwave'
 
 
 # Use Capistrano for deployment
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+  it { should have_many :attachments }
   it { should have_many(:answers).dependent(:destroy).order('best DESC') }
   it { should validate_length_of(:title).is_at_least(5).is_at_most(140) }
 end
